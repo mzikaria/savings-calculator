8bbcf83145114c72ab7067ce03b2fed8f0a5aefd
Mariam Zikaria <mzikaria@gmail.com>
2023-01-02 23:40:52 -0600

    Add inputs, button, and summary placeholders

--- src/App.js
+++ src/App.js
@@ -6,8 +6,20 @@ const App = () => {
   return (
     <div>
       <Title text="Savings Calculator"/>
-      <div className="inputs-container">[INPUTS_PLACEHOLDER]</div>
-      <div className="summary-container">[SUMMARY_PLACEHOLDER]</div>
+      <div className="inputs-container">[INPUTS_PLACEHOLDER]
+        <label>
+          MONTHS_PLACEHOLDER
+          <input type="text" name="months"/>
+        </label>
+        <label>
+          AMOUNT_PLACEHOLDER
+          <input type="text" name="amount"/>
+        </label>
+        <button>CALCULATE_BUTTON_PLACEHOLDER</button>
+      </div>
+      <div className="summary-container">[SUMMARY_PLACEHOLDER]
+        <h>SUMMARY_PLACEHOLDER</h>
+      </div>
       <div className="chart-container">[CHART_PLACEHOLDER]</div>
       <Footer text="Made by Mariam Zikaria"/>
     </div>
