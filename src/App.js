9daecfcfd1ebccda3ee5ebb750bc422d6b7377c3
Mariam Zikaria <mzikaria@gmail.com>
2023-01-03 23:11:13 -0600

    Add state logic to input fields

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
+import { useState } from 'react';
 import './App.css';
 import Title from './components/title';
 import Footer from './components/footer';
 import Chart from './components/chart';
 
 const App = () => {
+  const [months, setMonths] = useState(0);
+  const [amount, setAmount] = useState(0);
+
+  const handleChangeMonths = event => {
+    setMonths(event.target.value);
+  }
+
+  const handleChangeAmount = event => {
+    setAmount(event.target.value);
+  }
+
   return (
     <div>
       <Title text="Savings Calculator"/>
       <div className="inputs-container">
-        <input type="number" placeholder="Enter months" name="months"/>
-        <input type="number" placeholder="Enter Amount" name="amount"/>
+        <input
+          min="0"
+          type="number"
+          placeholder="Enter months"
+          value={months}
+          onChange={handleChangeMonths}
+        />
+        <input
+          min="0"
+          type="number"
+          placeholder="Enter amount"
+          value={amount}
+          onChange={handleChangeAmount}
+        />
         <button>Calculate</button>
       </div>
-      <div className="summary-container">[SUMMARY_PLACEHOLDER]
-        <h>SUMMARY_PLACEHOLDER</h>
+      <div className="summary-container">
+        <h1>savings: {months * amount}</h1>
       </div>
       <div className="chart-container">
         <Chart/>
